8ea921d3f3f09fe4b1baa4b60243a181b10644e5
Pedro Felix <pmhsfelix@gmail.com>
2019-01-02 18:35:57 +0000

    adds gradle based project

---
+++ src/test/kotlin/pt/ipl/isel/cc/kotlin/coroutines/CanaryTests.kt
@@ -0,0 +1,11 @@
+package pt.ipl.isel.cc.kotlin.coroutines
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class CanaryTests {
+    @Test
+    fun mustPass() {
+        assertTrue(true);
+    }
+}
